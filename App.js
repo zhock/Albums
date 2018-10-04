942d3e7a8d3f6775f41ea2d03fa52389ee477bc9
Elberth Bracho <elberthbracho@Elberths-Mac-mini.local>
2018-10-04 15:34:37 -0500

    Lista de albums

--- App.js
+++ App.js
@@ -1,46 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
   render() {
     return (
-      <Header headerText={'Albums'}/>
+      <View style={{ flex:1 }}>
+        <Header headerText={'Albums'}/>
+        <AlbumList />
+      </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
